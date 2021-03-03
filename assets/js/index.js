42a99fe367ad27dee939592795638743ae4a5732
kairen <s96113123@gmail.com>
2021-03-03 22:58:24 +0800

    finish

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,124 +1,161 @@
 "use strict";
 
-if (typeof NodeList.prototype.forEach !== 'function')  {
-    NodeList.prototype.forEach = Array.prototype.forEach;
+if (typeof NodeList.prototype.forEach !== "function") {
+  NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
-;(function () {
+(function () {
+  var mobileMenuList = document.querySelector(".mobileMenuList"),
+    overlay = document.querySelector(".overlay"),
+    searchContainer = document.querySelector(".searchContainer"),
+    deskRightSectionNav = document.querySelector(".deskRightSectionNav"),
+    iconClose = document.querySelector(".iconClose"),
+    iconHamburger = document.querySelector(".iconHamburger"),
+    deskSearch = document.querySelector(".deskSearch"),
+    deskSemobileWatchMorearch = document.querySelector(".mobileWatchMore"),
+    footerItemContainer = document.querySelector("footer .itemContainer"),
+    iconAngleUp = document.querySelector(".iconAngle.up"),
+    iconAngleDown = document.querySelector(".iconAngle.down"),
+    hamburgerIsOpen = false;
 
-    var mobileMenuList = document.querySelector(".mobileMenuList"),
-        overlay = document.querySelector(".overlay"),
-        searchContainer = document.querySelector(".searchContainer"),
-        deskRightSectionNav = document.querySelector(".deskRightSectionNav"),
-        iconClose = document.querySelector(".iconClose"),
-        iconHamburger = document.querySelector(".iconHamburger"),
-        deskSearch = document.querySelector(".deskSearch"),
-        deskSemobileWatchMorearch = document.querySelector(".mobileWatchMore"),
-        footerItemContainer = document.querySelector("footer .itemContainer"),
-        iconAngleUp = document.querySelector(".iconAngle.up"),
-        iconAngleDown = document.querySelector(".iconAngle.down"),
-        hamburgerIsOpen = false;
+  //mobile漢堡鈕
+  iconHamburger.addEventListener("click", function () {
+    hamburgerIsOpen = mobileMenuList.classList.contains("active");
+    mobileMenuList.classList[hamburgerIsOpen ? "remove" : "add"]("active");
+    mobileMenuList.classList[hamburgerIsOpen ? "add" : "remove"]("remove");
+    overlay.classList[hamburgerIsOpen ? "remove" : "add"]("overlayShow");
+  });
 
-    //mobile漢堡鈕
-    iconHamburger.addEventListener("click", function(){
+  function deskRightSectionNavToggle(flag) {
+    deskRightSectionNav.classList[flag ? "add" : "remove"]("show");
+    deskRightSectionNav.classList[flag ? "remove" : "add"]("hidden");
+    searchContainer.classList[flag ? "add" : "remove"]("hidden");
+    searchContainer.classList[flag ? "remove" : "add"]("show");
+  }
 
-        hamburgerIsOpen = mobileMenuList.classList.contains("active");
-        mobileMenuList.classList[hamburgerIsOpen ? "remove" : "add"]("active");
-        mobileMenuList.classList[hamburgerIsOpen ? "add" : "remove"]("remove");
-        overlay.classList[hamburgerIsOpen ? "remove" : "add"]("overlayShow");
-        
-    });
-    
-    function deskRightSectionNavToggle(flag){
-        deskRightSectionNav.classList[flag ? "add" : "remove" ]("show");
-        deskRightSectionNav.classList[flag ? "remove" : "add" ]("hidden");
-        searchContainer.classList[flag ? "add" : "remove" ]("hidden");
-        searchContainer.classList[flag ? "remove" : "add" ]("show");
-    }
+  // Desk搜尋按鈕
+  deskSearch.addEventListener("click", function () {
+    deskRightSectionNavToggle();
+  });
+  // Desk搜尋關閉
+  iconClose.addEventListener("click", function () {
+    deskRightSectionNavToggle("iconCloseByClick");
+  });
 
-    // Desk搜尋按鈕  
-    deskSearch.addEventListener("click", function () {
-        deskRightSectionNavToggle();
-    });
-    // Desk搜尋關閉
-    iconClose.addEventListener("click", function () {
-        deskRightSectionNavToggle("iconCloseByClick");
-    });
+  // overlay click
+  overlay.addEventListener("click", function () {
+    if (mobileMenuList.classList.contains("active")) iconHamburger.click();
+  });
 
-    // overlay click
-    overlay.addEventListener("click", function(){
-        if(mobileMenuList.classList.contains("active"))
-            iconHamburger.click();
-    });
+  // Mobile footer 了解更多
+  deskSemobileWatchMorearch.addEventListener("click", function () {
+    var footerItemContainerfFlag = footerItemContainer.classList.contains(
+      "displayShow"
+    );
+    footerItemContainer.classList[footerItemContainerfFlag ? "remove" : "add"](
+      "displayShow"
+    );
+    iconAngleUp.classList[footerItemContainerfFlag ? "add" : "remove"](
+      "displayShow"
+    );
+    iconAngleDown.classList[footerItemContainerfFlag ? "add" : "remove"](
+      "displayHidden"
+    );
+  });
 
-    // Mobile footer 了解更多
-    deskSemobileWatchMorearch.addEventListener("click", function(){
-        var footerItemContainerfFlag = footerItemContainer.classList.contains("displayShow");
-        footerItemContainer.classList[footerItemContainerfFlag ? "remove" : "add" ]("displayShow");
-        iconAngleUp.classList[footerItemContainerfFlag ? "add" : "remove" ]("displayShow");
-        iconAngleDown.classList[footerItemContainerfFlag ? "add" : "remove" ]("displayHidden");
-    });
+  // slideBox
+  var slideBox = document.querySelectorAll(".slideBox"),
+    prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    activeIndex = 0,
+    actionFlag = true,
+    firstFlag = true,
+    transitionSecond = "transform 0.5s";
 
-    // slideBox
-    var slideBox = document.querySelectorAll(".slideBox"),
-        activeIndex = 0,
-        slideBoxFlag = false,oldIndex = 0;
-
-    // slideAuto = setInterval(function(){
-    //     slideHandler("next");
-    // },2000);
-    var oldIndex = 0;
-    function slideHandler(type){
-        // window.clearInterval(slideAuto);
-        // 進場方向控制
-        slideBoxFlag = document.querySelector(".slideBox").classList.contains("rightToleft");
-        if((!slideBoxFlag && type === "next") | (slideBoxFlag && type === "prev")){
-            slideBox.forEach(function(element,index){
-                element.classList[(type === "next") ? "add" : "remove"]("rightToleft");
-            });
+  function slideHandler(type) {
+    if (!actionFlag) return;
+    if (type === "prev") {
+      slideBox.forEach(function (element, index) {
+        if (index !== activeIndex) {
+          slideBox[index].classList.remove("startRight");
+          slideBox[index].style.transition = "unset";
         }
-
-      
-        // slideBox.forEach(function(element,index){
-        //     if(oldIndex !== index){
-        //         slideBox[index].style.transition = "unset";
-        //         slideBox[index].classList.remove("rightToleft");
-        //     }
-        // });
-
-        // 動畫執行
-        setTimeout(function(){
-            slideBox[activeIndex].classList.remove("active");
-
-            slideBox[activeIndex].classList.add("rightToleft");
-            // slideBox[activeIndex].style.transition = "transform 0.5s";
-
-   
-            type === "next" ? (( activeIndex < slideBox.length - 1 ) ? activeIndex++ : activeIndex = 0) : (( activeIndex > 0 ) ? activeIndex-- : activeIndex = slideBox.length - 1);
-            // slideBox[activeIndex].classList.remove("rightToleft");
-            
-            slideBox[activeIndex].classList.add("active");
-           
-        });
+      });
+    } else {
+      slideBox.forEach(function (element, index) {
+        slideBox[index].classList.add("startRight");
+        slideBox[index].style.transition = "unset";
+      });
     }
 
-    document.querySelector(".prev").addEventListener("click",function(){
-        slideHandler("prev");
-    },false);
+    slideBox[activeIndex].style.transition = transitionSecond;
+    slideBox[activeIndex].classList[type === "prev" ? "add" : "remove"]("startRight");
+    slideBox[activeIndex].classList.remove("active");
 
-    document.querySelector(".next").addEventListener("click",function(){
-        slideHandler("next");
-    },false);
+    // 計算要顯示哪張slide
+    type === "next"
+      ? activeIndex < slideBox.length - 1
+        ? activeIndex++
+        : (activeIndex = 0)
+      : activeIndex > 0
+      ? activeIndex--
+      : (activeIndex = slideBox.length - 1);
 
+    if (type === "next" && activeIndex === 1 && firstFlag) {
+      //因首次執行ext時有動畫重疊bug故使用setTimeout
+      setTimeout(function () {
+        firstFlag = false;
+        slideBox[activeIndex].style.transition = transitionSecond;
+        slideBox[activeIndex].classList.add("startRight");
+        slideBox[activeIndex].classList.add("active");
+      }, 10);
+    } else {
+      slideBox[activeIndex].style.transition = transitionSecond;
+      slideBox[activeIndex].classList.add("active");
+    }
+  }
 
-    window.addEventListener("resize", function () {
-        mobileMenuList.classList.remove("active");
-        overlay.classList.remove("overlayShow");
-        iconClose.click();
-    });
+  // 管控動畫結束再執行下一動作
+  slideBox.forEach(function (element, index) {
+    element.addEventListener(
+      "transitionend",
+      function (e) {
+        actionFlag = true;
+      },
+      false
+    );
+    element.addEventListener(
+      "transitionstart",
+      function (e) {
+        actionFlag = false;
+      },
+      false
+    );
+  });
 
+  prev.addEventListener(
+    "click",
+    function () {
+      slideHandler("prev");
+    },
+    false
+  );
+
+  next.addEventListener(
+    "click",
+    function () {
+      slideHandler("next");
+    },
+    false
+  );
+
+  window.addEventListener("resize", function () {
+    mobileMenuList.classList.remove("active");
+    overlay.classList.remove("overlayShow");
+    iconClose.click();
+  });
 })();
 
 var direction = function direction() {
-    window.open("./page/crud.html", "_blank");
+  window.open("./page/crud.html", "_blank");
 };
